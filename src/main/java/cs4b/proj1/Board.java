cd3ba2a729565badf8896a27a822b5273207cfea
a-rosario <austin.t.rosario@gmail.com>
2019-02-04 22:23:41 -0800

    initial method for a check winner function and a print board board method (for debugging purposes until we get the actual user interface up and working)

--- src/main/java/cs4b/proj1/Board.java
+++ src/main/java/cs4b/proj1/Board.java
@@ -4,8 +4,65 @@ public class Board
 {
     private int[3][3] board;
 
-    boolean set(int player,   // the player's symbol
-                int x, int y) // position where to place player's symbol
+    public void consolPrint() // print the board to the consol for debuging purposes
+    {
+        for(int y = 0; y < 3; y++)
+        {
+            for(int x = 0; x < 3; x++)
+            {
+                System.out.print(board[y][x] + ", ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public int checkForWin()
+    {
+        for(int x = 0; x < 3; x++) // check all horizontal groups
+        {
+            // if there is a horizontal match AND that match is not empty spaces (empty space = 0)
+            if(board[0][x] != 0 && board[0][x] == board[1][x] == board[2][x])
+            {
+                return board[0][x]; // then return the winning token
+            }
+        }
+
+        for(int y = 0; y < 3; y++) // check all vertical groups
+        {
+            // if there is a vertical match AND that match is not empty spaces
+            if(board[y][0] != 0 && board[y][0] == board[y][1] == board[y][2])
+            {
+                return board[y][0]; // then return the winning token
+            }
+        }
+
+        if(board[0][0] != 0 && board[0][0] == board[1][1] == board[2][2]) // check both diagonal groups
+        {
+            return board[0][0];
+        }
+        else if(board[2][2] != 0 && board[2][2] == board[1][1] == board[0][0])
+        {
+            return board[2][2];
+        }
+        else // if no winner is found, then determine if there is still a playable/empty space
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (board[y][x] == 0) // if an empty space is found return empty token
+                    {
+                        return board[y][x]; // returning the empty token to show that the game is not over
+                    }
+                }
+            }
+        }
+
+        return -1; // if all other cases fail (no winner, and no empty spaces), then this game must be a draw
+    }
+
+    public boolean set(int player,   // the player's symbol
+                       int x, int y) // position where to place player's symbol
     {
         if(board[x][y] == 0) // if this board space is empty
         {
