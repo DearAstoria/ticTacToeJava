2b69a984b2fd75773729642c3357785706b221ea
justis ketcham <justis.ketcham@gmail.com>
2019-04-16 14:54:56 -0700

    included dependencies for postgreSQL and SQLite in gradle.build

--- out/production/resources/module-info.java
+++
@@ -1,9 +0,0 @@
-module TicTacToe {
-    requires javafx.fxml;
-    requires javafx.controls;
-    opens client;
-    opens client.gui_resources;
-    opens client.gui_controllers;
-    opens client.raw_data;
-    opens pubnubWrappers;
-}
